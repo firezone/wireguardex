51ba65a9a93e0ee2a3d2bf19b7b2760c3a4a0f9d
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2026-02-02 05:04:43 -0800

    chore: add root elixir files to formatter

--- checksum-Elixir.Wireguardex.exs
+++ checksum-Elixir.Wireguardex.exs
@@ -1,8 +1,14 @@
 %{
-  "libwireguard_nif-v0.5.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:56db4b2d9c737d0eebb24fbf5d65e7cf93c89f6060c76c103b5443e8744acaca",
-  "libwireguard_nif-v0.5.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:7aa9ac026aa32719e64ddaac14ee3230e560088dc2930e06e387fd3534ac3387",
-  "libwireguard_nif-v0.5.0-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:c28d8f7c721e3bbdacfd259ad887f3ca09bd34a7874dea92a3c6497345a2ebf8",
-  "libwireguard_nif-v0.5.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:9c305ed56b831b3ca996d23000373a1cd901fdc97d4d3ad4733b7fbc9a0c17bc",
-  "libwireguard_nif-v0.5.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:e1d12b15287c6bc68fec7ca243701dc4f9aa21c527d78ac1aa7b5e9ee1e3af66",
-  "libwireguard_nif-v0.5.0-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:ffdb46aaec8a635be11bfc6826972380c8b57188b99df11f0e2c9ca4d7b4c7f6",
+  "libwireguard_nif-v0.5.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" =>
+    "sha256:56db4b2d9c737d0eebb24fbf5d65e7cf93c89f6060c76c103b5443e8744acaca",
+  "libwireguard_nif-v0.5.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" =>
+    "sha256:7aa9ac026aa32719e64ddaac14ee3230e560088dc2930e06e387fd3534ac3387",
+  "libwireguard_nif-v0.5.0-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" =>
+    "sha256:c28d8f7c721e3bbdacfd259ad887f3ca09bd34a7874dea92a3c6497345a2ebf8",
+  "libwireguard_nif-v0.5.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" =>
+    "sha256:9c305ed56b831b3ca996d23000373a1cd901fdc97d4d3ad4733b7fbc9a0c17bc",
+  "libwireguard_nif-v0.5.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" =>
+    "sha256:e1d12b15287c6bc68fec7ca243701dc4f9aa21c527d78ac1aa7b5e9ee1e3af66",
+  "libwireguard_nif-v0.5.0-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" =>
+    "sha256:ffdb46aaec8a635be11bfc6826972380c8b57188b99df11f0e2c9ca4d7b4c7f6"
 }
